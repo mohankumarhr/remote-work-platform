0f334a0e44f6bfcaf564ada210fe173e363289e6
MohanKumar HR <mohankumarhr2003@gmail.com>
2026-01-03 18:38:35 +0530

    edited task service

---
+++ Backend/task-service/src/main/java/com/teamweave/taskservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.teamweave.taskservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleTypeMismatch() {
+        return ResponseEntity
+                .badRequest()
+                .body("Invalid or missing teamId");
+    }
+}
+
